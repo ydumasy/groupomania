165ed558d86ba15770957f8a09ec3b0c4ca51cf8
ydumasy <ydumasy@gmail.com>
2020-10-21 10:22:33 +0200

    Initialisation du serveur avec Node & Express

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Création de l'application Express
+const app = express();
+
+// Configuration des headers pour le CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+// Utilisation de bodyParser pour extraire l'objet JSON de la réponse
+app.use(bodyParser.json());
+
+// Implémentations des routes);
+app.get('/', (req, res) => {
+    res.json({ message: "L'application Express fonctionne" });
+});
+
+module.exports = app;
